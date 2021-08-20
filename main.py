ed948c3b148346fa33eeca9505ef5864590a106e
Lily Sam <lilyoforiwaas@gmail.com>
2021-08-20 17:21:26 -0500

    new directory changes

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
   
   #### Return a rendered index.html file
   return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
