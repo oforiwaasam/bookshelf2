3403d91d242d0936a47e07148ccce1e8636d65ad
Lily Sam <lilyoforiwaas@gmail.com>
2021-10-10 10:25:09 -0400

    added media for landing page

---
+++ website/static/js/user_landing.js
@@ -0,0 +1,47 @@
+// select DOM elements
+const time = document.getElementById('time'),
+    greeting = document.getElementById('greeting'),
+    name = document.getElementById('name'),
+    focus = document.getElementById('focus');
+
+// Show time
+function showTime() {
+    let today = new Date(),
+        // today.getHours returns a number between 0 and 23
+        hour = today.getHours(),
+        min = today.getMinutes(),
+        sec = today.getSeconds();
+
+    // set AM or PM
+    const amPm = hour >= 12 ? 'PM' : 'AM';
+
+    // 12hr format
+    hour = hour % 12 || 12;
+
+    //output the time
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+
+    // call showTime every sec(1000msec)
+    setTimeout(showTime, 1000);
+}
+
+// Add Zeros
+function addZero(n) {
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+// Set Background and Greeting
+function setBGreet() {
+    let today = new Date(),
+        hour = today.getHours();
+
+    if (hour < 12) {
+        // Morning
+        document.body.style.backgroundImage = "url(website/static/img/morning.jpeg)";
+    } else if (hour < 18) {
+        // Afternoon
+    }
+}
+
+// run showTime
+showTime();
